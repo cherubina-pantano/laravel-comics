908c45ea08ab5aa67d67a1dee42e16516ac05629
cherubina-pantano <cherubina.pantano@gmail.com>
2021-01-08 18:02:30 +0100

    CSS: main-home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,14 +2,14 @@
 
 @section('content')
 
-    <main class="wrap-home"> 
+    <main class="wrap-home mb-2"> 
         <section class="general-hero">
             <div class="container">
-                <img src="{{ asset('images/cover-home.jpg')}}" alt="">
+                <img class="cover-home"src="{{ asset('images/cover-home.jpg')}}" alt="">
             </div>
         </section>
 
-        <section class="comics-section mt-3 mb-3">
+        <section class="comics-section mt-3 mb-1">
             <div class="container">
                 <ul class="comics-list no-list-style">
                     @foreach ( $comics as $comic)
@@ -20,11 +20,58 @@
                             </a>
                         </li>
                     @endforeach
-
-                </ul>
-            
+                </ul>                
             </div>
-        
+            <div class="btn-container">
+                <button> LOAD MORE </button>        
+            </div>
+        </section>
+
+        <section class="dc-banner">
+            <div class="container">
+                <ul class="d-flex jc-sbetween no-list-style">
+                    <li>
+                        <div class="dc-features d-flex">
+                            <img src="{{ asset('images/digital-comics.png')}}" alt="">
+                            <a href="#">                            
+                                Digital Comics
+                            </a>
+                        </div>                        
+                    </li>
+                    <li>
+                        <div class="dc-features d-flex">
+                            <img src="{{ asset('images/merchandise.png')}}" alt="">                             
+                            <a href="#">
+                                 DC Merchandise                            
+                            </a>
+                        </div>                        
+                    </li>
+                    <li>
+                        <div class="dc-features d-flex">
+                            <img src="{{ asset('images/subscriptions.png')}}" alt="">                             
+                            <a href="#">
+                                Subscription                            
+                            </a>
+                        </div>                        
+                    </li>
+                    <li>
+                        <div class="dc-features d-flex">
+                            <img src="{{ asset('images/shop-locator.png')}}" alt="">                              
+                             <a href="#">
+                                 Comic Shop Locator                             
+                            </a>
+                        </div> 
+                    </li>
+                    <li>
+                        <div class="dc-features d-flex">
+                            <img src="{{ asset('images/power-visa.svg')}}" alt="">                              
+                            <a href="#">                            
+                                DC Power Visa                            
+                            </a>
+                        </div>                         
+                    </li>
+                </ul>
+            </div>
         </section>
     </main>
 
